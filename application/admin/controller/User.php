b224b6cb7504714b3abb1624034c46ab95156bba
zhengkai <network@iyuanma.net>
2017-05-25 01:39:06 +0800

    创建 /admin/controller/User.php 文件

---
+++ application/admin/controller/User.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @Date: 2017-05-24
+ * @Time: 23:08
+ * @Author: cdkay
+ * @Email: network@iyuanma.net
+ *
+ * @File： User.php
+ */
+
+namespace app\admin\controller;
+
+class User extends Base
+{
+    // TODO 用户管理 列表
+
+    // TODO 添加用户
+
+    // TODO 修改用户资料
+
+    // TODO 修改当前登录用户资料
+
+    // TODO 保存用户数据
+
+    // TODO 删除用户
+
+    // TODO 设置用户状态
+}
