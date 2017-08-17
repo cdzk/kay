8a59aca8acd994a5f56e00f9dad7e544c15cfcf3
zhengkai <network@iyuanma.net>
2017-08-17 23:04:31 +0800

    仓库异常处理

--- application/admin/view/public/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Powered by</b> 猿创创云 {$Think.SOFT_VERSION}
-    </div>
-    <strong>Copyright &copy;2016-2017 <a href="http://www.i-yc.com" target="_blank">成都猿创科技有限公司</a>.</strong> All rights
-    reserved.
-</footer>
